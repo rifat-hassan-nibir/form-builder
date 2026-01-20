8d8199d7cd16e1fa0bf567527e1a450e641c33ca
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-01-20 12:56:39 +0600

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Form Builder
+
+A form builder application.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Form Builder</h1>
+    </div>
+  );
+}
+
+export default App;
