16662272100c276054d2f15581851e071b294487
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-01-20 19:53:19 +0600

    feat: Implement initial application structure with React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Editor from "./components/editor/EditorLayout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/editor">
+          <Route index element={<Editor />} />
+          <Route path="/editor/:formId" element={<Editor />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
